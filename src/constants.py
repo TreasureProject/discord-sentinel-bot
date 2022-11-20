c644cdffd2a04e78efbf0c6edb76db42720aa3a6
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-11-20 08:56:28 -0800

    add prod server constants

--- src/constants.py
+++ src/constants.py
@@ -1,17 +1,27 @@
 ADMIN_ROLE = "Treasure Council"
 NEW_USER_ROLE = "Verified Hunter"
 
-PAUSE = 70
+PAUSE = 60
 EXPIRATION = 90
 
-SUPPORT_TICKETS_CHANNEL = 1034020144944521237
+# TEST
+# SUPPORT_TICKETS_CHANNEL = 1034020144944521237
+# NEW_USER_FLOW = [
+#     (1034020831069741076, "Welcome <@<user_id>>! Please read the start here information above."),
+#     (1034335732527026256, "<@<user_id>> make sure you have read the server rules!"),
+#     (1034020144579608616, "<@<user_id>> browse through the FAQ here for common questions."),
+#     (1034020144386678853, "<@<user_id>> select your notification roles here!"),
+#     (1034020144944521236, f"<@<user_id>> if you have any issues, ask about it here or create a ticket in <#{SUPPORT_TICKETS_CHANNEL}>"),
+#     (1034020144579608624, "<@<user_id>>, welcome to Treasure! Please give a brief introduction here!"),
+# ]
 
+# PROD
+SUPPORT_TICKETS_CHANNEL = 1031514582776283137
 NEW_USER_FLOW = [
-    (1034020831069741076, "Welcome <@<user_id>>! Please read the start here information above."),
-    (1034335732527026256, "<@<user_id>> make sure you have read the server rules!"),
-    (1034020144579608616, "<@<user_id>> browse through the FAQ here for common questions."),
-    (1034020145301045270, "<@<user_id>> join the conversations on our Cartridge Partners here!"),
-    (1034020144386678853, "<@<user_id>> select your notification roles here!"),
-    (1034020144944521236, f"<@<user_id>> if you have any issues, ask about it here or create a ticket in <#{SUPPORT_TICKETS_CHANNEL}>"),
-    (1034020144579608624, "<@<user_id>>, welcome to Treasure! Please give a brief introduction here!"),
+    (1034372214809702500, "Welcome <@<user_id>>! Please read the start here information above."),
+    (1034372457840246814, "<@<user_id>> make sure you have read the server rules!"),
+    (923030974533603348, "<@<user_id>> browse through the FAQ here for common questions."),
+    (1030544835909791815, "<@<user_id>> select your notification roles here!"),
+    (884676975489318932, f"<@<user_id>> if you have any issues, ask about it here or create a ticket in <#{SUPPORT_TICKETS_CHANNEL}>"),
+    (882872974972162118, "<@<user_id>>, welcome to Treasure! Please give a brief introduction here!"),
 ]
